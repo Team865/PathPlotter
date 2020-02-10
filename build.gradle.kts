0f55d1c9c656fd47f47d344bfc8de3a481190ccb
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-02-10 16:40:04 -0500

    Add network tables dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 @file:Suppress("UnusedImport", "SpellCheckingInspection")
 
+import org.gradle.internal.os.OperatingSystem
 import org.javamodularity.moduleplugin.extensions.TestModuleOptions
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -48,14 +49,30 @@ tasks.withType<Test> {
     }
 }
 
-val wpilibVersion = "2020.1.2"
+val wpilibVersion = "2020.2.2"
+
+fun desktopArch(): String {
+    val arch: String = System.getProperty("os.arch")
+    return if (arch == "amd64" || arch == "x86_64") "x86-64" else "x86"
+}
+
+fun desktopOS(): String {
+    return when {
+        OperatingSystem.current().isWindows -> "windows"
+        OperatingSystem.current().isMacOsX -> "osx"
+        else -> "linux"
+    }
+}
+
+val platform =  desktopOS() + desktopArch()
 
 dependencies {
     implementation(kotlin("stdlib"))
 
     implementation("edu.wpi.first.wpilibj:wpilibj-java:$wpilibVersion")
     implementation("edu.wpi.first.wpiutil:wpiutil-java:$wpilibVersion")
-//    implementation(files("ejml-combined-0.38.jar"))
+    implementation("edu.wpi.first.ntcore:ntcore-java:$wpilibVersion")
+    implementation("edu.wpi.first.ntcore:ntcore-jni:$wpilibVersion:$platform")
 
     testImplementation(kotlin("test"))
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = "5.5.1")
